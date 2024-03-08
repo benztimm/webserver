d89d2d37c5badf6330ab4a271496941f49702c64
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-03-08 00:06:07 -0800

    update webserver for accom finished request

--- webserver667/WebServer.java
+++ webserver667/WebServer.java
@@ -1,9 +1,19 @@
 package webserver667;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 import startup.configuration.MimeTypes;
 import startup.configuration.ServerConfiguration;
+import webserver667.requests.HttpMethods;
+import webserver667.requests.HttpRequest;
+import webserver667.requests.RequestReader;
 
 public class WebServer implements I667Server {
+  private ServerSocket serverSocket = null;
 
   @Override
   public void close() throws Exception {
@@ -11,7 +21,39 @@ public class WebServer implements I667Server {
 
   @Override
   public void start(ServerConfiguration configuration, MimeTypes mimeTypes) {
-    System.out.println(mimeTypes);
+    try {
+      serverSocket = new ServerSocket(configuration.getPort());
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        Thread requestThread = new Thread(() -> {
+          try {
+            RequestReader requestReader = new RequestReader(clientSocket.getInputStream());
+            HttpRequest method = requestReader.getRequest();
+            System.out.println(method.getHttpMethod());
+            System.out.println(method.getUri());
+            System.out.println(method.getVersion());
+            System.out.println(method.getHeader("Host"));
+            System.out.println(new String(method.getBody(), StandardCharsets.UTF_8));
+
+
+          } catch (Exception e) {
+            e.printStackTrace();
+          } finally {
+            try {
+              clientSocket.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+
+          }
+        });
+        requestThread.start();
+
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override

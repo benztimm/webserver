519f5c20a2be23e78c3ff6f396c01c698705a2e8
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-03-08 00:06:34 -0800

    finished resource

--- webserver667/responses/Resource.java
+++ webserver667/responses/Resource.java
@@ -1,35 +1,44 @@
 package webserver667.responses;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import startup.configuration.MimeTypes;
 import webserver667.requests.HttpRequest;
 import webserver667.responses.authentication.UserAuthenticator;
 
 public class Resource implements IResource {
-  public Resource(String uri, String queryString, String documentRoot, MimeTypes mimeTypes) {
+  private Path path;
+  private MimeTypes mimeTypes;
+  private String uri;
 
+  public Resource(String uri, String queryString, String documentRoot, MimeTypes mimeTypes) {
+    
+    this.mimeTypes = mimeTypes;
+    this.path = Paths.get(documentRoot.toString(), uri.toString()).normalize();
+    this.uri = uri;
   }
 
   @Override
   public boolean exists() {
-    throw new UnsupportedOperationException("Unimplemented method 'exists'");
+    return Files.exists(this.path);
   }
 
   @Override
   public Path getPath() {
-    throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+    return this.path;
   }
 
   @Override
   public boolean isProtected() {
-    throw new UnsupportedOperationException("Unimplemented method 'isProtected'");
+    return Files.exists(Paths.get(this.path.getParent().toString(),".passwords"));
   }
 
   @Override
   public boolean isScript() {
-    throw new UnsupportedOperationException("Unimplemented method 'isScript'");
+    return this.uri.contains("/scripts/");
   }
 
   @Override
@@ -39,22 +48,28 @@ public class Resource implements IResource {
 
   @Override
   public String getMimeType() {
-    throw new UnsupportedOperationException("Unimplemented method 'getMimeType'");
+    String fileName = path.getFileName().toString();
+    int i = fileName.lastIndexOf('.');
+    if (i > 0) {
+      String extension = fileName.substring(i + 1);
+      return mimeTypes.getMimeTypeByExtension(extension);
+    }
+    return "text/plain";
   }
 
   @Override
   public long getFileSize() throws IOException {
-    throw new UnsupportedOperationException("Unimplemented method 'getFileSize'");
+    return Files.size(path);
   }
 
   @Override
   public byte[] getFileBytes() throws IOException {
-    throw new UnsupportedOperationException("Unimplemented method 'getFileBytes'");
+    return Files.readAllBytes(path);
   }
 
   @Override
   public long lastModified() {
-    throw new UnsupportedOperationException("Unimplemented method 'lastModified'");
+    return path.toFile().lastModified();
   }
 
 }

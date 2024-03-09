810a1c1c32963bf7eae3a74c97aa553243dca9f3
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-03-08 18:28:36 -0800

    finished logger

--- webserver667/logging/Logger.java
+++ webserver667/logging/Logger.java
@@ -1,5 +1,10 @@
 package webserver667.logging;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import webserver667.requests.HttpMethods;
 import webserver667.requests.HttpRequest;
 import webserver667.responses.IResource;
 
@@ -13,6 +18,21 @@ public class Logger {
       IResource resource,
       int statusCode,
       int bytesSent) {
-    return "";
+
+
+    //String testUri = String.format("/%s", resource.getFileName());
+    String ipString = ipAddress;
+    String uri = request.getUri();
+    int fileSize = bytesSent;
+    String version = request.getVersion();
+    String method = request.getHttpMethod().toString();
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("PST"));
+    String dateTime = dateFormat.format(new Date());
+
+    
+    String outputString = ipString + " - - [" + dateTime + "] \"" + method + " " + uri + " " + version + "\" " + statusCode + " " + fileSize;
+    return outputString;
   }
 }

741cc42dd45f2503e27f2b54e5f47c252a074821
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-03-09 01:08:42 -0800

    finished forbiddenResponse

--- webserver667/responses/writers/ForbiddenResponseWriter.java
+++ webserver667/responses/writers/ForbiddenResponseWriter.java
@@ -1,19 +1,36 @@
 package webserver667.responses.writers;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import webserver667.requests.HttpRequest;
-
+import webserver667.responses.HttpResponseCode;
 import webserver667.responses.IResource;
 
 public class ForbiddenResponseWriter extends ResponseWriter {
-
+  private HttpRequest request;
+  private OutputStream outStream;
+  
   public ForbiddenResponseWriter(OutputStream out, IResource resource, HttpRequest request) {
     super(out, resource, request);
+    this.request = request;
+    this.outStream = out;
   }
 
   @Override
   public void write() {
-    throw new UnsupportedOperationException("Unimplemented method 'write'");
+    HttpResponseCode badRequest = HttpResponseCode.FORBIDDEN;
+    this.request.getVersion();
+    StringBuilder responseBuilder = new StringBuilder();
+    responseBuilder.append(
+        String.format("%s %d %s\r\n", this.request.getVersion(), badRequest.getCode(), badRequest.getReasonPhrase()));
+    String response = responseBuilder.toString();
+    System.out.println(response);
+    try {
+      this.outStream.write(response.getBytes());
+      this.outStream.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
